7619d0b8bc161760fb4b4881b204c37a8c26a98b
jhonjhsz2003-afk <jhonjhsz2003@gmail.com>
2026-09-23 23:17:43 -0300

    Delete NebulaTV directory

--- NebulaTV/src/main/java/br/com/nebulatv/app/AppState.java
+++
@@ -1,6 +0,0 @@
-package br.com.nebulatv.app;
-import br.com.nebulatv.model.*;
-public final class AppState {
-    private AppState(){}
-    public static Account account; public static Profile profile;
-}
--- NebulaTV/src/main/java/br/com/nebulatv/model/Account.java
+++
@@ -1,6 +0,0 @@
-package br.com.nebulatv.model;
-public class Account {
-    public long id; public String username; public String email; public String displayName; public boolean admin;
-    public Account() {}
-    public Account(long id,String username,String email,String displayName,boolean admin){this.id=id;this.username=username;this.email=email;this.displayName=displayName;this.admin=admin;}
-}
